32225f1c3f89aee0bf95d2e6f74e9e7ea2a562b2
kakkou <kakkou2001@gmail.com>
2018-04-24 12:48:40 +0900

    sample commit,

---
+++ Assets/MessagePackGenerated.cs
@@ -0,0 +1,223 @@
+﻿#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 168
+
+namespace MessagePack.Resolvers
+{
+    using System;
+    using MessagePack;
+
+    public class GeneratedResolver : global::MessagePack.IFormatterResolver
+    {
+        public static readonly global::MessagePack.IFormatterResolver Instance = new GeneratedResolver();
+
+        GeneratedResolver()
+        {
+
+        }
+
+        public global::MessagePack.Formatters.IMessagePackFormatter<T> GetFormatter<T>()
+        {
+            return FormatterCache<T>.formatter;
+        }
+
+        static class FormatterCache<T>
+        {
+            public static readonly global::MessagePack.Formatters.IMessagePackFormatter<T> formatter;
+
+            static FormatterCache()
+            {
+                var f = GeneratedResolverGetFormatterHelper.GetFormatter(typeof(T));
+                if (f != null)
+                {
+                    formatter = (global::MessagePack.Formatters.IMessagePackFormatter<T>)f;
+                }
+            }
+        }
+    }
+
+    internal static class GeneratedResolverGetFormatterHelper
+    {
+        static readonly global::System.Collections.Generic.Dictionary<Type, int> lookup;
+
+        static GeneratedResolverGetFormatterHelper()
+        {
+            lookup = new global::System.Collections.Generic.Dictionary<Type, int>(4)
+            {
+                {typeof(global::MessagePack.MessagePackType), 0 },
+                {typeof(global::NoGeneratedEnum), 1 },
+                {typeof(global::GeneratedEnum), 2 },
+                {typeof(global::TestObj), 3 },
+            };
+        }
+
+        internal static object GetFormatter(Type t)
+        {
+            int key;
+            if (!lookup.TryGetValue(t, out key)) return null;
+
+            switch (key)
+            {
+                case 0: return new MessagePack.Formatters.MessagePack.MessagePackTypeFormatter();
+                case 1: return new MessagePack.Formatters.NoGeneratedEnumFormatter();
+                case 2: return new MessagePack.Formatters.GeneratedEnumFormatter();
+                case 3: return new MessagePack.Formatters.TestObjFormatter();
+                default: return null;
+            }
+        }
+    }
+}
+
+#pragma warning disable 168
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
+
+#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 168
+
+namespace MessagePack.Formatters.MessagePack
+{
+    using System;
+    using MessagePack;
+
+    public sealed class MessagePackTypeFormatter : global::MessagePack.Formatters.IMessagePackFormatter<global::MessagePack.MessagePackType>
+    {
+        public int Serialize(ref byte[] bytes, int offset, global::MessagePack.MessagePackType value, global::MessagePack.IFormatterResolver formatterResolver)
+        {
+            return MessagePackBinary.WriteByte(ref bytes, offset, (Byte)value);
+        }
+        
+        public global::MessagePack.MessagePackType Deserialize(byte[] bytes, int offset, global::MessagePack.IFormatterResolver formatterResolver, out int readSize)
+        {
+            return (global::MessagePack.MessagePackType)MessagePackBinary.ReadByte(bytes, offset, out readSize);
+        }
+    }
+
+
+}
+
+#pragma warning disable 168
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
+#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 168
+
+namespace MessagePack.Formatters
+{
+    using System;
+    using MessagePack;
+
+    public sealed class NoGeneratedEnumFormatter : global::MessagePack.Formatters.IMessagePackFormatter<global::NoGeneratedEnum>
+    {
+        public int Serialize(ref byte[] bytes, int offset, global::NoGeneratedEnum value, global::MessagePack.IFormatterResolver formatterResolver)
+        {
+            return MessagePackBinary.WriteInt32(ref bytes, offset, (Int32)value);
+        }
+        
+        public global::NoGeneratedEnum Deserialize(byte[] bytes, int offset, global::MessagePack.IFormatterResolver formatterResolver, out int readSize)
+        {
+            return (global::NoGeneratedEnum)MessagePackBinary.ReadInt32(bytes, offset, out readSize);
+        }
+    }
+
+    public sealed class GeneratedEnumFormatter : global::MessagePack.Formatters.IMessagePackFormatter<global::GeneratedEnum>
+    {
+        public int Serialize(ref byte[] bytes, int offset, global::GeneratedEnum value, global::MessagePack.IFormatterResolver formatterResolver)
+        {
+            return MessagePackBinary.WriteInt32(ref bytes, offset, (Int32)value);
+        }
+        
+        public global::GeneratedEnum Deserialize(byte[] bytes, int offset, global::MessagePack.IFormatterResolver formatterResolver, out int readSize)
+        {
+            return (global::GeneratedEnum)MessagePackBinary.ReadInt32(bytes, offset, out readSize);
+        }
+    }
+
+
+}
+
+#pragma warning disable 168
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
+
+
+#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 168
+
+namespace MessagePack.Formatters
+{
+    using System;
+    using MessagePack;
+
+
+    public sealed class TestObjFormatter : global::MessagePack.Formatters.IMessagePackFormatter<global::TestObj>
+    {
+
+        public int Serialize(ref byte[] bytes, int offset, global::TestObj value, global::MessagePack.IFormatterResolver formatterResolver)
+        {
+            if (value == null)
+            {
+                return global::MessagePack.MessagePackBinary.WriteNil(ref bytes, offset);
+            }
+            
+            var startOffset = offset;
+            offset += global::MessagePack.MessagePackBinary.WriteFixedArrayHeaderUnsafe(ref bytes, offset, 2);
+            offset += global::MessagePack.MessagePackBinary.WriteNil(ref bytes, offset);
+            offset += formatterResolver.GetFormatterWithVerify<global::GeneratedEnum>().Serialize(ref bytes, offset, value.MyProperty, formatterResolver);
+            return offset - startOffset;
+        }
+
+        public global::TestObj Deserialize(byte[] bytes, int offset, global::MessagePack.IFormatterResolver formatterResolver, out int readSize)
+        {
+            if (global::MessagePack.MessagePackBinary.IsNil(bytes, offset))
+            {
+                readSize = 1;
+                return null;
+            }
+
+            var startOffset = offset;
+            var length = global::MessagePack.MessagePackBinary.ReadArrayHeader(bytes, offset, out readSize);
+            offset += readSize;
+
+            var __MyProperty__ = default(global::GeneratedEnum);
+
+            for (int i = 0; i < length; i++)
+            {
+                var key = i;
+
+                switch (key)
+                {
+                    case 1:
+                        __MyProperty__ = formatterResolver.GetFormatterWithVerify<global::GeneratedEnum>().Deserialize(bytes, offset, formatterResolver, out readSize);
+                        break;
+                    default:
+                        readSize = global::MessagePack.MessagePackBinary.ReadNextBlock(bytes, offset);
+                        break;
+                }
+                offset += readSize;
+            }
+
+            readSize = offset - startOffset;
+
+            var ____result = new global::TestObj();
+            ____result.MyProperty = __MyProperty__;
+            return ____result;
+        }
+    }
+
+}
+
+#pragma warning disable 168
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using MessagePack;
+
+// don't generate formatter because not used in messagepack-object type.
+public enum NoGeneratedEnum {
+    Foo, Bar
+}
+
+public enum GeneratedEnum {
+    Foo, Bar
+}
+
+[MessagePackObject]
+public class TestObj {
+    [Key( 1 )]
+    public GeneratedEnum MyProperty { get; set; }
+}
